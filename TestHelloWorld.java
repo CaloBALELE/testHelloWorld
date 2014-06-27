2324b416d8d6b5d9ff3bbdbe9c36447c592824df
CaloBALELE <carlosoliveira_94@hotmail.com>
2014-06-27 11:42:14 +0100

    Update TestHelloWorld.java

--- TestHelloWorld.java
+++ TestHelloWorld.java
@@ -1,12 +1,14 @@
 public class TestHelloWorld {
 	
 	public static void main(String[] args) {
-		
+		//CALO ESTEVE AQUI!!!!
 		System.out.println("Hello World!!!");
 		
 		System.out.println("Hello Universe!!!");
-		
+		System.out.println("HELOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO! :D");
+		System.out.println("BALELELEEEEEEEEEEEEEEEEEEEEEEEE");
 		// above code is added by ARC-tester
+		//CALO ESTEVE AQUI MWAHAHAHA
 		String str = "*";
 		for (int i = 0; i < 10; i++) {
 			System.out.println(str);
